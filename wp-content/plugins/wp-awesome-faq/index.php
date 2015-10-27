2968eab9fa7d425e6cc5cf232121e5a64769898b
woonjjang <woonjjang@ufofactory.org>
2015-10-27 10:47:08 +0900

    [plug-in] wp-awesome-faq 3.0.2 update

--- wp-content/plugins/wp-awesome-faq/index.php
+++ wp-content/plugins/wp-awesome-faq/index.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Awesome FAQ
 Plugin URI: http://jeweltheme.com/product/wp-awesome-faq-pro/
 Description: Accordion based Awesome WordPress FAQ Plugin
-Version: 3.0.1
+Version: 3.0.2
 Author: Liton Arefin
 Author URI: http://www.jeweltheme.com
 License: GPL2
